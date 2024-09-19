603ad819ef24656ef96f2c0b7072bcfe7384b5a8
Artyom <tiom.teomin@yandex.ru>
2024-09-19 23:04:16 +0300

    Команда старт

---
+++ func_main.py
@@ -0,0 +1,7 @@
+from aiogram import types
+import text_answer
+import logging
+# Функция приветсвия по команде /start
+async def start_bot(messega: types.Message):
+    logging.info('COMMAND "START" username - ' + messega.from_user.username)
+    await messega.answer(text=text_answer.START_BOT)
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import logging
 
+from aiogram import Bot, Dispatcher, executor
+
+import config
+import func_main
+bot = Bot(config.BOT_TOKEN)
+dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO, filemode='w', filename='py.log',
                     format='%(asctime)s | %(levelname)s | %(message)s')
 
 
+
+dp.message_handler(commands=['start'])(func_main.start_bot)
+
 def main():
     logging.info('START BOT')
+    executor.start_polling(dp)
 
 
 if __name__ == '__main__':
