d27945501996b3f153deef261844d89931f6a3b6
Artyom <tiom.teomin@yandex.ru>
2024-09-22 11:53:28 +0300

    Настройка запроса на получении фото

--- getIMG.py
+++ getIMG.py
@@ -1,18 +1,53 @@
-import requests
+import time
+
 import config
-URL = f'https://api-key.fusionbrain.ai/key/api/v1/text2image/run'
+import requests
+import json
+class Text2ImageAPI:
+    def __init__(self, url, api_key, secret_key):
+        self.URL = url
+        self.AUTH_HEADERS = {
+            'X-Key': f'Key {api_key}',
+            'X-Secret': f'Secret {secret_key}',
+        }
 
-params = {
-    "type": "GENERATE",
-    "numImages": "1",
-    "width": "1024",
-    "height": "1024",
-    "generateParams": {
-        "query": f"Котики на мотоциклах"
-    }
-}
+    def get_model(self):
+        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+        data = response.json()
+        print(data)
+        return data[0]['id']
 
+    def generate(self, prompt, model, images=1, width=1024, height=1024):
+        params = {
+            "type": "GENERATE",
+            "numImages": images,
+            "width": width,
+            "height": height,
+            "generateParams": {
+                "query": f"{prompt}"
+            }
+        }
+        data = {
+            'model_id': (None, model),
+            'params': (None, json.dumps(params), 'application/json')
+        }
+        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        data = response.json()
+        print(data)
+        return data['uuid']
 
-def get_image():
-    # requ = requests.get()
-    pass
+    def generation_image(self, request_id, attemps=10, delay=10):
+        while attemps > 0:
+            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            data = response.json()
+            print(data)
+            if data['status'] == 'DONE':
+                return data['images']
+            attemps -= 1
+            time.sleep(delay)
+
+api = Text2ImageAPI('https://api-key.fusionbrain.ai/', config.API_KANDINSKY, config.SECRET_KEY)
+model_id = api.get_model()
+uuid = api.generate("Кот на мотоцикле", model_id)
+images = api.generation_image(uuid)
+print(images)
