b1556b1388786ac68cb798402827feaa892b6eea
Artyom <59234653+ArtyomRoboti@users.noreply.github.com>
2024-10-14 21:04:56 +0300

    Update like_func.py
    Правки для работы на удалённом ПК

--- function/like_func.py
+++ function/like_func.py
@@ -2,12 +2,12 @@ import logging
 
 from aiogram import types, Bot
 
-from IMGi_bot import actions
-from IMGi_bot import config
-from IMGi_bot import getIMG
-from IMGi_bot import keyboards
-from IMGi_bot import text_answer
-from IMGi_bot.DB import db_photo, db_check_like, db_set_img, db_rating, db_technikal, db_tech_image
+import actions
+import config
+import getIMG
+import keyboards
+import text_answer
+from DB import db_photo, db_check_like, db_set_img, db_rating, db_technikal, db_tech_image
 
 bot = Bot(config.BOT_TOKEN)
 
