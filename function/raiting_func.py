f97a003c9092e16cf34c991479af00632c71afd8
Artyom <59234653+ArtyomRoboti@users.noreply.github.com>
2024-10-14 21:05:16 +0300

    Update raiting_func.py
    Правки для работы на удалённом ПК

--- function/raiting_func.py
+++ function/raiting_func.py
@@ -2,11 +2,11 @@ import logging
 
 from aiogram import types, Bot
 
-from IMGi_bot import actions
-from IMGi_bot import config
-from IMGi_bot import keyboards
-from IMGi_bot import text_answer
-from IMGi_bot.DB import db_photo, db_user, db_set_img, db_rating, db_technikal, db_tech_image
+import actions
+import config
+import keyboards
+import text_answer
+from DB import db_photo, db_user, db_set_img, db_rating, db_technikal, db_tech_image
 
 bot = Bot(config.BOT_TOKEN)
 
